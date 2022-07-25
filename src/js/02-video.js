74be5eafc8b49f335653a632c933f5a7470f2f29
Anna-Chumakova <ann020187@gmail.com>
2022-07-25 21:16:56 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector("#vimeo-player");
+
+const player = new Player(iframe);
+player.on(
+    "timeupdate", throttle(function (time) {
+    console.log(time);
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+    }), 1000);
+savedTime();
+function savedTime() {
+    const savedTimeVideo = localStorage.getItem('videoplayer-current-time');
+    const parcedsavedTimeVideo = JSON.parse(savedTimeVideo);
+    if (savedTimeVideo) {
+       player.setCurrentTime(parcedsavedTimeVideo.seconds); 
+    }
+
+}
