ded946979f2b4fd3a839ddc58a289b87b21bb39e
Anna-Chumakova <ann020187@gmail.com>
2022-07-26 21:02:49 +0300

    task3 по Репеті

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,40 +2,36 @@ import throttle from "lodash.throttle";
 
 const STORAGE_KEY = "feedback-form-state";
 
-const inputEl = document.querySelector("input");
-const textareaEl = document.querySelector("textarea");
 const formEl = document.querySelector(".feedback-form");
-
-let formData = {};
+populateInput();
 
 formEl.addEventListener("submit", onFormSend);
 formEl.addEventListener("input", throttle(onMessageSave, 500))
 
-populateInput();
-
-function onMessageSave(evt) {
-    formData[evt.target.name] = evt.target.value;
-    
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));   
-}
-
 function onFormSend(evt) {
     evt.preventDefault();
-    console.log(formData);
+    const formData = new FormData(formEl);
+    formData.forEach((value, name) => 
+        console.log(value, name)
+    )
     evt.currentTarget.reset();
-   
     localStorage.removeItem(STORAGE_KEY);   
-    formData = { email: "", message: "" };
+    
+}
+
+function onMessageSave(evt) {
+    let persistedFilters = localStorage.getItem(STORAGE_KEY);
+    persistedFilters = persistedFilters ? JSON.parse(persistedFilters) : {};
+    persistedFilters[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(persistedFilters));   
 }
 
 function populateInput() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
-    const parsedSavedMessage = JSON.parse(savedMessage);
-    
-  if (savedMessage) {
-      
-      inputEl.value = parsedSavedMessage.email;
-      textareaEl.value = parsedSavedMessage.message;
-      formData = {email: parsedSavedMessage.email, message: parsedSavedMessage.message}
+    let persistedFilters = localStorage.getItem(STORAGE_KEY); 
+  if (persistedFilters) {
+      persistedFilters = JSON.parse(persistedFilters);
+      Object.entries(persistedFilters).forEach(([name, value]) => {
+          formEl.elements[name].value = value;
+      })
    }
 }
